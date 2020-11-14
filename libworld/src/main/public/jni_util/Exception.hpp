2c09114f8a594ef5a204d0d286a6a53795a2f2ff
kokkiemouse <kokkiemouse@gmail.com>
2020-11-14 13:25:17 +0900

    added world
    Signed-off-by: kokkiemouse <kokkiemouse@gmail.com>

---
+++ libworld/src/main/public/jni_util/Exception.hpp
@@ -0,0 +1,48 @@
+
+#ifndef JNI_UTIL_EXCEPTION_HPP
+#define JNI_UTIL_EXCEPTION_HPP
+
+#include <jni.h>
+#include <cassert>
+#include <exception>
+#include <stdexcept>
+
+namespace world_class
+{
+
+class JavaException : public std::exception { };
+
+inline void throwRuntimeException(JNIEnv* env, const char* what)
+{
+    jclass classj = env->FindClass("Ljava/lang/RuntimeException:");
+    if (classj == nullptr)
+        return;
+    
+    env->ThrowNew(classj, what);
+    env->DeleteLocalRef(classj);
+}
+
+template <typename Func>
+inline void convertException(JNIEnv* env, Func func) noexcept
+{
+    try {
+        func();
+    } catch (JavaException& e) {
+        return;
+    } catch (std::exception& e) {
+        throwRuntimeException(env, e.what());
+    } catch (...) {
+        throwRuntimeException(env, "Unindentified exception thrown (not derived from std::exception)");
+    }
+}
+
+inline void checkException(JNIEnv* env)
+{
+    if (env->ExceptionOccurred())
+        throw JavaException();
+}
+
+}
+
+#endif
+
---
+++ src/main/java/world/cpp/Synthesis.java
@@ -0,0 +1,5 @@
+package world.cpp;
+
+public class Synthesis {
+    
+}
